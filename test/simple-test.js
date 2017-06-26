a798e5208362fa59daade13ed8355102d97bf050
teramotodaiki <i101323@gmail.com>
2017-06-26 11:43:56 +0900

    colors

--- test/simple-test.js
+++ test/simple-test.js
@@ -3,16 +3,25 @@ const { resolve } = require('path');
 const { exec } = require('child_process');
 const { output } = require('../webpack.config.js');
 
+const colors = require('colors/safe');
+
+colors.setTheme({
+  RE: 'yellow',
+  WA: 'yellow',
+  AC: ['green', 'bold']
+});
+
 // Start test
 const scriptPath = resolve(output.path, output.filename);
 exec(`node ${scriptPath} < ./input`, (error, stdout, stderr) => {
   if (stderr) {
-    console.log('Runtime Error. (See ./stderr)');
+    console.log(colors.RE('Runtime Error'), 'See ./stderr');
     writeFileSync('./stderr', stderr);
   } else if (stdout !== readFileSync('./answer', 'utf8')) {
-    console.log('Wrong Answer. (See ./stdout)');
+    console.log(colors.WA('Wrong Answer'), 'See ./stdout');
     writeFileSync('./stdout', stdout);
   } else {
-    console.log('Accepted!');
+    console.log(colors.AC('Accepted!'));
+    writeFileSync('./stdout', stdout);
   }
 });
